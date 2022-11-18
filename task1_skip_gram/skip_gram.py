c982c3540f89948a38669923fb3f6edb1d295a67
Galaxyjie <75266632@qq.com>
2022-11-18 10:39:27 +0800

    update skip_gram

--- task1_skip_gram/skip_gram.py
+++ task1_skip_gram/skip_gram.py
@@ -1,5 +1,3 @@
-# Defined in Section 5.2.3.1
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -10,24 +8,27 @@ from tqdm.auto import tqdm
 from utils import BOS_TOKEN, EOS_TOKEN, PAD_TOKEN
 from utils import load_reuters, save_pretrained, get_loader, init_weights
 
-class Skip_GramDataset(Dataset):
+
+class Skip_Gram_Dataset(Dataset):
     def __init__(self, corpus, vocab, context_size=2):
         self.data = []
         self.bos = vocab[BOS_TOKEN]
         self.eos = vocab[EOS_TOKEN]
         for sentence in tqdm(corpus, desc="Dataset Construction"):
-            sentence = [self.bos] + sentence+ [self.eos]
+            sentence = [self.bos] + sentence + [self.eos]
+            # 如句子长度不足以构建（当前词、目标上下文）训练样本，则跳过
             if len(sentence) < context_size * 2 + 1:
                 continue
             for i in range(context_size, len(sentence) - context_size):
-                for j in range(context_size):
-                    context = sentence[i]
-                    target = sentence[i - context_size + j]
-                    self.data.append((context, target))
-                for j in range(context_size):
-                    context = sentence[i]
-                    target = sentence[i + j + 1]
-                    self.data.append((context, target))
+                # 模型输入：当前词
+                current_word = sentence[i]
+                # 模型输出：左右分别取context_size长度的上下文
+                for j in range(i - context_size, i + context_size + 1):
+                    if j == i:
+                        continue
+                    target = sentence[j]
+                    self.data.append((current_word, target))
+
     def __len__(self):
         return len(self.data)
 
@@ -39,12 +40,13 @@ class Skip_GramDataset(Dataset):
         targets = torch.tensor([ex[1] for ex in examples])
         return (inputs, targets)
 
-class Skip_GramModel(nn.Module):
+
+class Skip_Gram_Model(nn.Module):
     def __init__(self, vocab_size, embedding_dim):
-        super(Skip_GramModel, self).__init__()
-        # 词嵌入层
+        super(Skip_Gram_Model, self).__init__()
+        # 词嵌入层 提示：nn.Embedding
         self.embeddings = nn.Embedding(vocab_size, embedding_dim)
-        # 线性变换
+        # 线性变换：隐含层->输出层 提示：nn.Linear
         self.output = nn.Linear(embedding_dim, vocab_size)
         init_weights(self)
 
@@ -52,7 +54,8 @@ class Skip_GramModel(nn.Module):
         embeds = self.embeddings(inputs)
         output = self.output(embeds)
         log_probs = F.log_softmax(output, dim=1)
-        return log_probs
+        return log_probs  # log_probs是概率分布
+
 
 embedding_dim = 64
 context_size = 2
@@ -61,21 +64,21 @@ num_epoch = 10
 
 # 读取文本数据，构建Skip_Gram模型训练数据集
 corpus, vocab = load_reuters()
-dataset = Skip_GramDataset(corpus, vocab, context_size=context_size)
+dataset = Skip_Gram_Dataset(corpus, vocab, context_size=context_size)
 data_loader = get_loader(dataset, batch_size)
 
 nll_loss = nn.NLLLoss()
 # 构建Skip_Gram模型，并加载至device
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = Skip_GramModel(len(vocab), embedding_dim)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = Skip_Gram_Model(len(vocab), embedding_dim)
 model.to(device)
 optimizer = optim.Adam(model.parameters(), lr=0.02)
 
 model.train()
-for epoch in range(1, num_epoch+1):
+for epoch in range(1, num_epoch + 1):
     total_loss = 0
     for batch in tqdm(data_loader, desc=f"Training Epoch {epoch}"):
-        inputs, targets = [x.to(device) for x in batch]  
+        inputs, targets = [x.to(device) for x in batch]
         optimizer.zero_grad()
         log_probs = model(inputs)
         loss = nll_loss(log_probs, targets)
@@ -84,5 +87,7 @@ for epoch in range(1, num_epoch+1):
         total_loss += loss.item()
     if epoch % 1 == 0:
         # 保存词向量（model.embeddings）
-        save_pretrained(vocab, model.embeddings.weight.data, "data/skip_gram.vec"+str(epoch))
+        save_pretrained(
+            vocab, model.embeddings.weight.data, "data/skip_gram.vec" + str(epoch)
+        )
     print(f"Loss: {total_loss:.2f}")
